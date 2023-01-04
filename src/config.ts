dba76492316d58242b8cbcdf80c121413d42fab6
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-01-04 11:31:21 +0900

    feat: add eslint-plugin-unicorn

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'node:fs'
 
 export const PATH = {
   config: process.env.CONFIG_PATH || './config.json',
--- src/notified.ts
+++ src/notified.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'node:fs'
 import { PATH } from './config'
 
 export class Notified {
