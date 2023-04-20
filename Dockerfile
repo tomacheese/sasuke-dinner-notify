53b969a243b60e39f1945cf11970371e1e82833a
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-04-20 22:05:17 +0900

    feat: #113 twapiからの脱却

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,4 @@
-FROM node:19
-
-RUN apt-get update && \
-  apt-get upgrade -y && \
-  apt-get clean \
-  && rm -rf /var/lib/apt/lists/*
+FROM node:19-alpine as builder
 
 WORKDIR /app
 
@@ -14,15 +9,52 @@ RUN echo network-timeout 600000 > .yarnrc && \
   yarn install --frozen-lockfile && \
   yarn cache clean
 
-COPY entrypoint.sh .
-RUN chmod +x entrypoint.sh
-
 COPY src src
 COPY tsconfig.json .
 
-ENV NODE_ENV=production
-ENV CONFIG_PATH=/data/config.json
-ENV NOTIFIED_PATH=/data/notified.json
+RUN yarn package
+
+FROM alpine:edge as runner
+
+# hadolint ignore=DL3018
+RUN apk update && \
+  apk add --no-cache dumb-init && \
+  apk add --no-cache curl fontconfig font-noto-cjk && \
+  fc-cache -fv && \
+  apk add --no-cache \
+  chromium \
+  nss \
+  freetype \
+  freetype-dev \
+  harfbuzz \
+  ca-certificates \
+  ttf-freefont \
+  nodejs \
+  yarn \
+  xvfb \
+  xauth \
+  dbus \
+  dbus-x11 \
+  x11vnc \
+  && \
+  apk add --update --no-cache tzdata && \
+  cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime && \
+  echo "Asia/Tokyo" > /etc/timezone && \
+  apk del tzdata
+
+WORKDIR /app
+
+COPY --from=builder /app/output .
+
+COPY entrypoint.sh .
+RUN chmod +x entrypoint.sh
+
+ENV DISPLAY :99
+ENV NODE_ENV production
+ENV CONFIG_PATH /data/config.json
+ENV NOTIFIED_PATH /data/notified.json
+ENV CHROMIUM_PATH /usr/bin/chromium-browser
 ENV LOG_DIR /data/logs
 
-ENTRYPOINT [ "/app/entrypoint.sh" ]
+ENTRYPOINT ["dumb-init", "--"]
+CMD ["/app/entrypoint.sh"]
--- src/config.ts
+++ src/config.ts
@@ -1,111 +1,55 @@
-import fs from 'node:fs'
-import { Logger } from '@book000/node-utils'
+import { ConfigFramework } from '@book000/node-utils'
 
 export const PATH = {
-  config: process.env.CONFIG_PATH || './config.json',
   notified: process.env.NOTIFIED_PATH || './notified.json',
 }
 
-export interface ProxyConfig {
-  host: string
-  port: number
-  username?: string
-  password?: string
-  protocol?: string
+export interface TwitterAccount {
+  username: string
+  password: string
+  authCodeSecret?: string
+  targetUserId: string
 }
 
 export interface Configuration {
-  twitter?: {
-    consumerKey: string
-    consumerSecret: string
-    targetUserId: string
-  }
-  twapi?: {
-    baseUrl: string
-    basicUsername: string
-    basicPassword: string
-    targetUserId: string
-  }
+  twitter: TwitterAccount
   discord: {
     token: string
     channelId: string
   }
-  proxy?: ProxyConfig
 }
-
-const isConfig = (config: any): config is Configuration => {
-  const logger = Logger.configure('isConfig')
-  const checks: {
-    [key: string]: boolean
-  } = {
-    'config is defined': !!config,
-    'twitter or twapi is defined': !!config.twitter || !!config.twapi,
-    'discord is defined': !!config.discord,
-    'discord.token is defined': !!config.discord.token,
-    'discord.token is string': typeof config.discord.token === 'string',
-    'discord.token is not empty': config.discord.token.trim() !== '',
-    'discord.channelId is defined': !!config.discord.channelId,
-    'discord.channelId is string': typeof config.discord.channelId === 'string',
-    'discord.channelId is not empty': config.discord.channelId.trim() !== '',
-  }
-  if (config.twitter) {
-    checks['twitter.consumerKey is defined'] = !!config.twitter.consumerKey
-    checks['twitter.consumerKey is string'] =
-      typeof config.twitter.consumerKey === 'string'
-    checks['twitter.consumerKey is not empty'] =
-      config.twitter.consumerKey.trim() !== ''
-
-    checks['twitter.consumerSecret is defined'] =
-      !!config.twitter.consumerSecret
-    checks['twitter.consumerSecret is string'] =
-      typeof config.twitter.consumerSecret === 'string'
-    checks['twitter.consumerSecret is not empty'] =
-      config.twitter.consumerSecret.trim() !== ''
-
-    checks['twitter.targetUserId is defined'] = !!config.twitter.targetUserId
-    checks['twitter.targetUserId is string'] =
-      typeof config.twitter.targetUserId === 'string'
-    checks['twitter.targetUserId is not empty'] =
-      config.twitter.targetUserId.trim() !== ''
-  }
-  if (config.twapi) {
-    checks['twapi.baseUrl is defined'] = !!config.twapi.baseUrl
-    checks['twapi.baseUrl is string'] = typeof config.twapi.baseUrl === 'string'
-    checks['twapi.baseUrl is not empty'] = config.twapi.baseUrl.trim() !== ''
-
-    checks['twapi.basicUsername is defined'] = !!config.twapi.basicUsername
-    checks['twapi.basicUsername is string'] =
-      typeof config.twapi.basicUsername === 'string'
-
-    checks['twapi.basicPassword is defined'] = !!config.twapi.basicPassword
-    checks['twapi.basicPassword is string'] =
-      typeof config.twapi.basicPassword === 'string'
-
-    checks['twapi.targetUserId is defined'] = !!config.twapi.targetUserId
-    checks['twapi.targetUserId is string'] =
-      typeof config.twapi.targetUserId === 'string'
-    checks['twapi.targetUserId is not empty'] =
-      config.twapi.targetUserId.trim() !== ''
-  }
-  const result = Object.values(checks).every(Boolean)
-  if (!result) {
-    logger.error('Invalid config. Missing check(s):')
-    for (const [key, value] of Object.entries(checks)) {
-      if (!value) {
-        logger.error(`- ${key}`)
-      }
+export class SDNConfiguration extends ConfigFramework<Configuration> {
+  protected validates(): { [key: string]: (config: Configuration) => boolean } {
+    return {
+      'config is defined': (config) => !!config,
+      'twitter is defined': (config) => !!config.twitter,
+      'twitter.username is defined': (config) => !!config.twitter.username,
+      'twitter.username is string': (config) =>
+        typeof config.twitter.username === 'string',
+      'twitter.username is not empty': (config) =>
+        config.twitter.username.trim() !== '',
+      'twitter.password is defined': (config) => !!config.twitter.password,
+      'twitter.password is string': (config) =>
+        typeof config.twitter.password === 'string',
+      'twitter.password is not empty': (config) =>
+        config.twitter.password.trim() !== '',
+      'twitter.targetUserId is defined': (config) =>
+        !!config.twitter.targetUserId,
+      'twitter.targetUserId is string': (config) =>
+        typeof config.twitter.targetUserId === 'string',
+      'twitter.targetUserId is not empty': (config) =>
+        config.twitter.targetUserId.trim() !== '',
+      'discord is defined': (config) => !!config.discord,
+      'discord.token is defined': (config) => !!config.discord.token,
+      'discord.token is string': (config) =>
+        typeof config.discord.token === 'string',
+      'discord.token is not empty': (config) =>
+        config.discord.token.trim() !== '',
+      'discord.channelId is defined': (config) => !!config.discord.channelId,
+      'discord.channelId is string': (config) =>
+        typeof config.discord.channelId === 'string',
+      'discord.channelId is not empty': (config) =>
+        config.discord.channelId.trim() !== '',
     }
   }
-  return result
-}
-
-export function getConfig(): Configuration {
-  if (!fs.existsSync(PATH.config)) {
-    throw new Error('Config file not found')
-  }
-  const config = JSON.parse(fs.readFileSync(PATH.config, 'utf8'))
-  if (!isConfig(config)) {
-    throw new Error('Invalid config')
-  }
-  return config
 }
